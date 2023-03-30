2a7945c5e9746947c3862b8d429f13584ce2f476
aserlite <78406876+aserlite@users.noreply.github.com>
2023-03-31 00:27:27 +0100

    Create db.sql

---
+++ src/db.sql
@@ -0,0 +1,246 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.4deb1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le : ven. 31 mars 2023 à 01:22
+-- Version du serveur : 8.0.32-0ubuntu0.22.10.2
+-- Version de PHP : 8.1.7-1ubuntu3.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `JavaISP`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `boats`
+--
+
+CREATE TABLE `boats` (
+  `id` int NOT NULL,
+  `type` text NOT NULL,
+  `name` text NOT NULL,
+  `maker` text NOT NULL,
+  `registration` text NOT NULL,
+  `color` text,
+  `engine` text,
+  `number_passengers` int DEFAULT NULL,
+  `stock` int NOT NULL,
+  `price` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cars`
+--
+
+CREATE TABLE `cars` (
+  `id` int NOT NULL,
+  `type` text NOT NULL,
+  `name` text NOT NULL,
+  `maker` text NOT NULL,
+  `registration` text NOT NULL,
+  `color` text,
+  `engine` text,
+  `number_passengers` int DEFAULT NULL,
+  `stock` int NOT NULL,
+  `price` int NOT NULL,
+  `wheels` int DEFAULT NULL,
+  `number_doors` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `planes`
+--
+
+CREATE TABLE `planes` (
+  `id` int NOT NULL,
+  `type` text NOT NULL,
+  `name` text NOT NULL,
+  `maker` text NOT NULL,
+  `registration` text NOT NULL,
+  `color` text,
+  `engine` text,
+  `number_passengers` int DEFAULT NULL,
+  `stock` int NOT NULL,
+  `price` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `rental`
+--
+
+CREATE TABLE `rental` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `vehicle_id` int NOT NULL,
+  `start_date` date NOT NULL,
+  `duration` int NOT NULL,
+  `rental_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `trucks`
+--
+
+CREATE TABLE `trucks` (
+  `id` int NOT NULL,
+  `type` text NOT NULL,
+  `name` text NOT NULL,
+  `maker` text NOT NULL,
+  `registration` text NOT NULL,
+  `color` text,
+  `engine` text,
+  `number_passengers` int DEFAULT NULL,
+  `stock` int NOT NULL,
+  `price` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `name` text NOT NULL,
+  `email` text NOT NULL,
+  `password` text NOT NULL,
+  `admin` tinyint(1) DEFAULT NULL,
+  `account_creation` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `vehicles`
+--
+
+CREATE TABLE `vehicles` (
+  `id` int NOT NULL,
+  `type` text NOT NULL,
+  `name` text NOT NULL,
+  `maker` text NOT NULL,
+  `registration` text NOT NULL,
+  `color` text,
+  `engine` text,
+  `number_passengers` int DEFAULT NULL,
+  `stock` int NOT NULL,
+  `price` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `boats`
+--
+ALTER TABLE `boats`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `cars`
+--
+ALTER TABLE `cars`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `planes`
+--
+ALTER TABLE `planes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `rental`
+--
+ALTER TABLE `rental`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `trucks`
+--
+ALTER TABLE `trucks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `vehicles`
+--
+ALTER TABLE `vehicles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `boats`
+--
+ALTER TABLE `boats`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `cars`
+--
+ALTER TABLE `cars`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `planes`
+--
+ALTER TABLE `planes`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `rental`
+--
+ALTER TABLE `rental`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `trucks`
+--
+ALTER TABLE `trucks`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `vehicles`
+--
+ALTER TABLE `vehicles`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
