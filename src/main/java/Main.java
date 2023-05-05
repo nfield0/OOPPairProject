e7ecebe6a49fc1954dbc6001b8bf231ca7506c65
Nathan <nathanfield32@gmail.com>
2023-05-05 18:12:46 +0100

    :art: Structured main testing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -23,25 +23,47 @@ public class Main {
 
         Dealer dealer = new Dealer(1,"Mercedes","Gaia","048104");
 
+        //Users
         //insertUser(0,"John","john@gmail.com","Password1",0);
         findAllUsers();
+
+
+        //Dealers
         //insertDealer("Mercedes Dundalk","Dundalk, Co.Louth","087-7741222");
         findAllDealers();
-        // insertCar("Hatchback","Volkswagen","Golf","1.6L tdi", "124lk00","White",2.5,5,10000,10000,"Diesel",dealer,"",5);
+
+
+        //Cars
+        CarDaoInterface carDao = new MySqlCarDao();
+        Car c = new Car(0,"Volkswagen","Golf","1.6L tdi", "124lk00","White",2.5,5,10000,10000,"Diesel",dealer,"",5 );
+        //insertCar(c);
         findAllCars();
         System.out.println("Find one");
         findCarById(1);
+        carDao.deleteById(1);
 
+        //Boats
+        Boat b = new Boat(0,"Bayliner", "2455 Ciera", "MerCruiser 5.7L", "FL2345", "White", 2.5, 6, 450, 30000, "Gasoline", dealer, "", 1, 30);
+        //insertBoat(b);
         findAllBoats();
 
 
+        //Trucks
+        Truck t = new Truck(0,"Volvo", "VNL 760", "Volvo D13TC", "TX1234", "Blue", 18.1, 2, 500000, 120000, "Diesel", dealer, "", 80000);
+        //insertTruck(t);
         findAllTrucks();
 
+
+        //Planes
+
+        Plane p = new Plane(0,"Boeing", "737 MAX", "CFM International", "N12345", "White", 79.0, 189, 4850, 120000000, "Jet A", dealer, "", 2, 3700, 470, 220);
+        //insertPlane(p);
         findAllPlanes();
 
 
-
     }
+
+
 
     private static void findCarById(int id) throws DaoException {
         CarDaoInterface carDao = new MySqlCarDao();
@@ -89,12 +111,43 @@ public class Main {
             throw new RuntimeException(e);
         }
     }
-    private static void insertCar(String type,String make, String model, String engine, String registration, String color, double weightInTonnes, int numPassengers, int mileage, int price, String fuelType, Dealer dealer, String imgUrl,int numDoors)
+
+    private static void insertCar(Car c)
     {
         CarDaoInterface carDao = new MySqlCarDao();
         try
         {
-            carDao.insertCar(type,make,model,engine,registration,color,weightInTonnes,numPassengers,mileage,price,fuelType,dealer,imgUrl, numDoors);
+            carDao.insertCar(c.getMake(),c.getModel(),c.getEngine(),c.getRegistration(),c.getColor(),c.getWeightInTonnes(),c.getNumPassengers(),c.getMileage(),c.getPrice(),c.getFuelType(),c.getDealer(),c.getImgUrl(),c.getNumDoors());
+        } catch (DaoException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private static void insertTruck(Truck v)
+    {
+        TruckDaoInterface dao = new MySqlTruckDao();
+        try
+        {
+            dao.insertTruck(v.getMake(),v.getModel(),v.getEngine(),v.getRegistration(),v.getColor(),v.getWeightInTonnes(),v.getNumPassengers(),v.getMileage(),v.getPrice(),v.getFuelType(),v.getDealer(),v.getImgUrl(),v.getWeight_capacity());
+        } catch (DaoException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private static void insertBoat(Boat v)
+    {
+        BoatDaoInterface dao = new MySqlBoatDao();
+        try
+        {
+            dao.insertBoat(v.getMake(),v.getModel(),v.getEngine(),v.getRegistration(),v.getColor(),v.getWeightInTonnes(),v.getNumPassengers(),v.getMileage(),v.getPrice(),v.getFuelType(),v.getDealer(),v.getImgUrl(),v.getNumLifeBoats(),v.getMax_speed_knots());
+        } catch (DaoException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private static void insertPlane(Plane v)
+    {
+        PlaneDaoInterface dao = new MySqlPlaneDao();
+        try
+        {
+            dao.insertPlane(v.getMake(),v.getModel(),v.getEngine(),v.getRegistration(),v.getColor(),v.getWeightInTonnes(),v.getNumPassengers(),v.getMileage(),v.getPrice(),v.getFuelType(),v.getDealer(),v.getImgUrl(),v.getNumEngines(),v.getRange(),v.getMax_speed_knots(),v.getSeating_capacity());
         } catch (DaoException e) {
             throw new RuntimeException(e);
         }
